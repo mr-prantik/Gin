9cadfa3ec3da5f69e1437cf5094277a186f39706
mr-prantik <prantikmahata9211@gmail.com>
2023-10-29 22:45:14 +0530

    updated

--- mychat/mychat/asgi.py
+++ mychat/mychat/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for gin project.
+ASGI config for mychat project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gin.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mychat.settings')
 
 application = get_asgi_application()
